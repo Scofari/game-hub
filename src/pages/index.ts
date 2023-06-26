9300b4754ffc4cc991e1fba80102607e85f421f7
Vladimir <scofari99@gmail.com>
2023-06-26 19:54:44 +0300

    Handle errors

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,6 @@
 import HomePage from "./HomePage";
 import Layout from "./Layout";
 import GameDetail from "./GameDetail";
+import ErrorPage from "./ErrorPage";
 
-export { HomePage, Layout, GameDetail };
+export { HomePage, Layout, GameDetail, ErrorPage };
