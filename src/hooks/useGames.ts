e3bc125fdf776c4f06c856f10b4d8fe0150ca866
Vladimir <scofari99@gmail.com>
2023-06-26 14:29:01 +0300

    Refactor: simplify time calculations

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,5 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
+import ms from "ms";
 import { FetchResponse } from "../types/fetch-response";
 import { GameQuery } from "../types/game-query.interface";
 import { Game } from "../types/game.interface";
@@ -26,7 +27,7 @@ const useGames = (gameQuery: GameQuery) => {
 					page: pageParam,
 				},
 			}),
-		staleTime: 24 * 60 * 60 * 1000, //24h
+		staleTime: ms("24h"),
 		getNextPageParam: (lastPage, allPages) => {
 			return lastPage.next ? allPages.length + 1 : undefined;
 		},
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
 import genres from "../data/genres";
 import { Genre } from "../types/genre.interface";
 import APIClient from "./../services/apiClient";
@@ -9,7 +10,7 @@ const useGenres = () =>
 	useQuery({
 		queryKey: ["genres"],
 		queryFn: apiClient.getAll,
-		staleTime: 24 * 60 * 60 * 1000, //24h
+		staleTime: ms("24h"),
 		initialData: genres,
 	});
 
