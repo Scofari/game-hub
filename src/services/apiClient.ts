cfba8b18e6891232aab846c84ab71c46b2bd1c68
Vladimir <scofari99@gmail.com>
2023-06-30 19:32:30 +0300

    Delete react.svg icon

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,34 +1,34 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
-	count: number;
-	next: string | null;
-	results: T[];
+  count: number;
+  next: string | null;
+  results: T[];
 }
 
 const axiosInstance = axios.create({
-	baseURL: "https://api.rawg.io/api",
-	params: {
-		key: "6c873c54c5bf484391644b487ceba4fb",
-	},
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "6c873c54c5bf484391644b487ceba4fb",
+  },
 });
 
 class APIClient<T> {
-	endpoint: string;
+  endpoint: string;
 
-	constructor(endpoint: string) {
-		this.endpoint = endpoint;
-	}
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
 
-	getAll = (config: AxiosRequestConfig) =>
-		axiosInstance
-			.get<FetchResponse<T>>(this.endpoint, config)
-			.then((response) => response.data);
+  getAll = (config: AxiosRequestConfig) =>
+    axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((response) => response.data);
 
-	get = (id: number | string) =>
-		axiosInstance
-			.get<T>(`${this.endpoint}/${id}`)
-			.then((response) => response.data);
+  get = (id: number | string) =>
+    axiosInstance
+      .get<T>(`${this.endpoint}/${id}`)
+      .then((response) => response.data);
 }
 
 export default APIClient;
